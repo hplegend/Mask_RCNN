0b36d0eb361814f5e3b2d12ae8f4a3bf9db77333
潇鹤 <hepeng0123@126.com>
2021-12-26 17:38:48 +0800

    modify

--- samples/coco/training_finish_callback.py
+++ samples/coco/training_finish_callback.py
@@ -1,27 +1,17 @@
 # 保存训练点
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import Callback
 
 
-class TrainingResultCheckpoint(ModelCheckpoint):
+class TrainingResultCheckpoint(Callback):
     def __init__(self, filepath, monitor='val_loss', verbose=0,
                  save_best_only=False, save_weights_only=False,
-                 mode='auto', period=1, training_args=None, meta=None):
-        super(TrainingResultCheckpoint, self).__init__(filepath,
-                                                       monitor=monitor,
-                                                       verbose=verbose,
-                                                       save_best_only=save_best_only,
-                                                       save_weights_only=save_weights_only,
-                                                       mode=mode,
-                                                       period=period)
+                 mode='auto', period=1):
+        super(TrainingResultCheckpoint, self).__init__()
 
         self.filepath = filepath
         self.new_file_override = True
-        self.meta = meta or {'epochs': [], self.monitor: []}
-
-        if training_args:
-            self.meta['training_args'] = training_args
 
     def on_train_end(self, logs=None):
-        self.model.save(self.filepath, overwrite=True)
+        self.model.save_weight(self.filepath, overwrite=True)
 
         pass
